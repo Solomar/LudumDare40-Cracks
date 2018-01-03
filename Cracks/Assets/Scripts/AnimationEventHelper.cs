e847dd82d9e4917d4370e74a3d8803dc0696a3fa
Pong <>
2018-01-03 01:16:26 -0500

    Summary added

--- Cracks/Assets/Scripts/AnimationEventHelper.cs
+++ Cracks/Assets/Scripts/AnimationEventHelper.cs
@@ -2,6 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// This script let's you call functions from any script as an animation event as
+/// long as this script is on the same object as the animator and that the script
+/// with the wanted function has a reference to it to be able to add the function
+/// in AnimationEventHelper's delegate.
+/// </summary>
 public class AnimationEventHelper : MonoBehaviour {
 
     // Keeping these not static in case I want to use them for other
